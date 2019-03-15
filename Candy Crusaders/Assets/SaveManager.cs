ff715c607bbd39b05d57b187d36cc71fdd8e7c15
Michael Bian <mbian3@uwo.ca>
2019-03-15 11:49:54 -0400

    save functionality

---
+++ Candy Crusaders/Assets/SaveManager.cs
@@ -0,0 +1,45 @@
+﻿//Repurpsed from "Unity Mobile Game -Saving - Android & IOS [C#][Tutorial]
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveManager : MonoBehaviour
+{
+    public static SaveManager Instance { set; get; }
+    public SaveState state;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        Instance = this;
+        Load();
+
+        Debug.Log(Helper.Serialize<SaveState>(state));
+    }
+
+    //Saves state to player pref
+    public void Save()
+    {
+        PlayerPrefs.SetString("save", Helper.Serialize<SaveState>(state));
+    }
+
+
+
+
+
+    //Load the previous saved state from the player prefs
+    public void Load()
+    {
+        //Check if the player already has a save
+        if (PlayerPrefs.HasKey("save"))
+        {
+            state = Helper.Deserialize<SaveState>(PlayerPrefs.GetString("save"));
+        }
+        else
+        {
+            state = new SaveState();
+            Save();
+            Debug.Log("No save file found. Creating a new one.");
+        }
+    }
+}
