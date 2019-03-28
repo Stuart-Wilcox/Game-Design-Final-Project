68f8448014f424283745a37334880afcb2a74f05
Jimmy <jtangtra@uwo.ca>
2019-03-28 10:58:10 -0400

    Created main menu

---
+++ Candy Crusaders/Assets/MainMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayCampaign()
+    {
+        Debug.Log("Play Campaign");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Characters()
+    {
+        Debug.Log("Characters");
+        SceneManager.LoadScene("Characters");
+    }
+
+    public void Gacha()
+    {
+        Debug.Log("Gacha");
+        SceneManager.LoadScene("Gacha");
+    }
+
+    public void Events()
+    {
+        Debug.Log("Events");
+        SceneManager.LoadScene("Events");
+    }
+
+    //exit the game
+    public void QuitGame()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+}
