5fafd55d98cceb4098a8c570b1d1f54aa0ebaaf6
Stuart-Wilcox <stuart_wilcox@outlook.com>
2019-03-30 17:42:39 -0400

    level 1 working

---
+++ Candy Crusaders/Assets/Level1/Scripts/Turn.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turn : ScriptableObject
+{
+    public bool isMove;
+    public bool isAttack;
+    public Vector2 movePosition;
+    public Vector2 attackPosition;
+}
