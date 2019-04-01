3d66e3e02396873bf51c22ed898e78b7168c33fd
unknown <agoston.the.dev@gmail.com>
2019-04-01 15:16:34 -0400

    Add sounds to menu and combat

--- Candy Crusaders/Assets/Level1/Scripts/AttackService.cs
+++ Candy Crusaders/Assets/Level1/Scripts/AttackService.cs
@@ -6,11 +6,13 @@ public class AttackService : MonoBehaviour
 {
     private Character[] characters;
     private Character[] enemies;
+    private AudioSource audioSource;
 
-    public AttackService(Character[] characters, Character[] enemies)
+    public AttackService(Character[] characters, Character[] enemies, AudioSource source)
     {
         this.characters = characters;
         this.enemies = enemies;
+        this.audioSource = source;
     }
 
     public void PlayerAttack(int playerIndex, int enemyIndex)
@@ -19,6 +21,7 @@ public class AttackService : MonoBehaviour
         int damage = Mathf.Min(this.enemies[enemyIndex].hp, this.characters[playerIndex].atk - this.enemies[enemyIndex].def);
         damage = Mathf.Max(1, damage); // make sure at least 1 damage per hit
         this.enemies[enemyIndex].hp -= damage;
+        audioSource.Play();
     }
 
     public void EnemyAttack(int playerIndex, int enemyIndex)
@@ -27,5 +30,6 @@ public class AttackService : MonoBehaviour
         int damage = Mathf.Min(this.characters[playerIndex].hp, this.enemies[enemyIndex].atk - this.characters[playerIndex].def);
         damage = Mathf.Max(1, damage); // make sure at least 1 damage per hit
         this.characters[playerIndex].hp -= damage;
+        audioSource.Play();
     }
 }
