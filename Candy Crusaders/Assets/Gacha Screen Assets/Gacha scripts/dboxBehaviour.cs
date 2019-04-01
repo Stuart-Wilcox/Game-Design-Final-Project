aeb7180036c994f8cbd6f59ede3f211f807ad7eb
Michael Bian <yanglong117@gmail.com>
2019-04-01 13:16:02 -0400

    gacha

--- Candy Crusaders/Assets/Gacha Screen Assets/Gacha scripts/dboxBehaviour.cs
+++ Candy Crusaders/Assets/Gacha Screen Assets/Gacha scripts/dboxBehaviour.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class dboxBehaviour : MonoBehaviour
 {
+    private GameObject unit;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,7 @@ public class dboxBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        unit = GameObject.FindGameObjectWithTag("0");
+        unit.SetActive(true);
     }
 }
